ed07145ac43c22c9eb6e144834f0cda05149dab5
Leila Feiguin <leila_fei@hotmail.com>
2017-06-05 20:12:00 -0300

    Agrego funcion mostrar graficamente polos y ceros

--- procesadorDeFunciones.m
+++ procesadorDeFunciones.m
@@ -23,9 +23,11 @@ function [expresion, ceros, polos, ganancia] = obtenerExpresionConPolosCerosYGan
   [ceros, polos, ganancia]=tf2zp(coeficientesNumerador, coeficientesDenominador)
 endfunction  
 
+function = mostrarGraficamentePolosYCeros(funcionTransferencia)
+  pzmap(funcionTransferencia)
+  endfunction
 
 %Falta:
-%6. Mostrar gráficamente la distribución de polos y ceros.
 %7. Indicar estabilidad del sistema.
 %8. Obtener todas las características de la función transferencia: Esta funcionalidad barre todos los puntos anteriores (1, 2, 3, 4, 5,6 y 7).
 %9. Ingresar una nueva función.
